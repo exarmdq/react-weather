57d458f57edae20b4f58ad4aa9607c81fc3d43cb
Exar Adrian Valenzuela <exarmdq@gmail.com>
2021-07-27 19:16:22 -0300

    agregar eslinter y config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  settings: { react: { version: '14.17.3' } },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+}
